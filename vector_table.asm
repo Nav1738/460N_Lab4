84c13735701890063c3fd536774cbd983bb929f9
Nav1738 <arnavvats7@gmail.com>
2023-03-07 15:36:20 -0600

    Second done commit

--- vector_table.asm
+++ vector_table.asm
@@ -1,4 +1,4 @@
-.ORIG x0202 ;Double check for NOP for next line
+.ORIG x0200 ;Double check for NOP for next line
 .FILL x1200 ; Timer interrupt
 .FILL x1600; Protection exception 
 .FILL x1A00; unaligned access exception
